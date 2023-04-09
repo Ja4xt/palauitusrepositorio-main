4326c7fe2778e234076beeaabb45a066a007d77d
Ja4xt <jan.maxx46@gmail.com>
2023-04-09 21:03:57 +0300

    puhelinluettelo update

--- palauitusrepositorio-main/palauitusrepositorio-main/osa2/puhelinluettelo/src/App.js
+++ palauitusrepositorio-main/palauitusrepositorio-main/osa2/puhelinluettelo/src/App.js
@@ -1,31 +1,111 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
-import PersonList from "./components/PersonList";
-//import Filter from "./components/Filter";
+import Persons from "./components/Persons";
+import personService from "./services/persons";
+import Notification from "./components/Notification";
+
 /*
 JM fullstackopen 2023 
 puhelinluettelo
 */
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
-  ]);
-  const [newFilter, setNewFilter] = useState("");
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(newFilter.toLowerCase())
-  );
+  const [persons, setPersons] = useState([]);
+  const [newPerson, setNewPerson] = useState({ name: "", number: "" });
+  const [filter, setFilter] = useState("");
+  const [personsToShow, setPersonsToShow] = useState([]);
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
+      setPersonsToShow(initialPersons);
+    });
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const currentName = persons.filter(
+      (person) => person.name === newPerson.name
+    );
+
+    if (currentName.length === 0) {
+      personService
+        .create(newPerson)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+          setPersonsToShow(persons.concat(returnedPerson));
+          setMessage(`Added ${newPerson.name} to phonebook`);
+        })
+        .catch((error) => setMessage(error.response.data.error));
+    } else {
+      if (
+        window.confirm(
+          `${newPerson.name} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        personService
+          .update(currentName[0].id, newPerson)
+          .then((returnedPerson) => {
+            const updatedPersons = persons.map((person) =>
+              person.id !== returnedPerson.id ? person : returnedPerson
+            );
+            setPersons(updatedPersons);
+            setPersonsToShow(updatedPersons);
+            setMessage(`Updated ${newPerson.name}'s number`);
+          })
+          .catch((error) => setMessage(error.response.data.error));
+      }
+    }
+    setNewPerson({ name: "", number: "" });
+  };
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService.remove(id).then((response) => {
+        const updatedPersons = persons.filter((person) => person.id !== id);
+        setPersons(updatedPersons);
+        setPersonsToShow(updatedPersons);
+        setMessage(`Removed ${name} from phonebook`);
+      });
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewPerson({ ...newPerson, [name]: value });
+  };
+
+  const filterByName = (event) => {
+    const search = event.target.value;
+    setFilter(search);
+    setPersonsToShow(
+      persons.filter((person) => person.name.toLowerCase().includes(search))
+    );
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* <Filter newFilter={newFilter} setNewFilter={setNewFilter} /> */}
-      <h3>Add a new</h3>
-      <PersonForm persons={persons} setPersons={setPersons} />
-      <h3>Numbers</h3>
-      <PersonList persons={filteredPersons} />
+      <Notification message={message} />
+      <Filter filter={filter} filterByName={filterByName} />
+      <PersonForm
+        addPerson={addPerson}
+        newPerson={newPerson}
+        handleChange={handleChange}
+      />
+      <h2>Numbers</h2>
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   );
 };
