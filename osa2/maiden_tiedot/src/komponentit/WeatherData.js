0cbd33f6b9aecaf06deeb7816a1c8f08ed8cf1d6
ja4xt <jan.mattila@centria.fi>
2023-08-29 16:00:10 +0300

    i am just trying stuff

--- osa2/maiden_tiedot/src/komponentit/WeatherData.js
+++ osa2/maiden_tiedot/src/komponentit/WeatherData.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import axios from "axios";
 
 const WeatherData = ({ city }) => {
-  const OPENWEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  //const OPENWEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  //const OPENWEATHER_API_KEY = import.meta.env.VITE_SOME_KEY;
+  const OPENWEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY
   const [weather, setWeather] = useState([]);
 
   useEffect(() => {
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPENWEATHER_API_KEY}`
-        //`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_key}`
+        //`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${OPENWEATHER_API_KEY}`
       )
       .then((response) => {
         setWeather(response.data);
@@ -27,9 +29,51 @@ const WeatherData = ({ city }) => {
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
           />
           <div>Wind {weather.wind.speed} m/s</div>
+          <div>Nosub {weather.lat}</div>
+          <div>Nosub {weather.lon}</div>
+          <div>Nosub {weather.part}</div>
         </div>
       ) : null}
     </>
+  );
+};
+
+export default WeatherData;*/
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const WeatherData = ({ city }) => {
+  const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  }, [city]);
+
+  if (!weather) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <div>Temperature {weather.main.temp}°C</div>
+      <img
+        alt="weather icon"
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+      />
+      <div>Wind {weather.wind.speed} m/s</div>
+    </div>
   );
 };
 
